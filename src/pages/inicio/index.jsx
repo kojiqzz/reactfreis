6da264643cdf1d843e127a7e85713b302de34daa
kojiqzz <victorhugosantos360@gmail.com>
2024-08-05 16:37:29 -0300

    design

--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -33,17 +33,24 @@ export default function Inicio() {
         <div className="cards">
           <Link to="/ex2">
             <div className="card">
-              <div className="retangulo laranja" />
-              <h3 className="titulo">Cupom de desconto</h3>
-              <h4 className="subtitulo">Exercício 1</h4>
+              <div className="retangulo verde" />
+              <h3 className="titulo">Gramas para KG</h3>
+              <h4 className="subtitulo">Exercício 2</h4>
+            </div>
+          </Link>
+        </div>
+      </div>
+        <div className="card3">
+          <Link to="/ex3">
+            <div className="card">
+              <div className="retangulo azul" />
+              <h3 className="titulo">Gramas para KG</h3>
+              <h4 className="subtitulo">Exercício 2</h4>
             </div>
           </Link>
         </div>
       </div>
 
    
-
-
-    </div>
-  );
+  )
 }
